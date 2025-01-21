844040d1fd4bc94335feb44d63c95d646c4b8a60
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-01-22 04:30:09 +0900

    [Add] 2014번: 소수의곱

--- src/heap/Boj2014.java
+++ src/heap/Boj2014.java
@@ -1,5 +1,43 @@
 package heap;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 public class Boj2014 {
 
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int K = Integer.parseInt(st.nextToken());
+    int N = Integer.parseInt(st.nextToken());
+
+    int[] arr = new int[K];
+    PriorityQueue<Long> pq = new PriorityQueue<>();
+    HashSet<Long> set = new HashSet<>();
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < K; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+      pq.offer((long) arr[i]);
+      set.add((long) arr[i]);
+    }
+
+    long answer = 0;
+    for (int cnt = 0; cnt < N; cnt++) {
+      answer = pq.poll();
+      for (int i = 0; i < K; i++) {
+        long next = answer * arr[i];
+        if (!set.contains(next)) {
+          pq.offer(next);
+          set.add(next);
+        }
+        if (answer % arr[i] == 0) break;
+      }
+    }
+    System.out.println(answer);
+  }
 }
