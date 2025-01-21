2a9b72da46a40a7dda6055f96b2a49fdfbc65ffa
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-01-21 16:43:49 +0900

    [Add] 11286번: 절댓값힙

--- src/heap/Boj11286.java
+++ src/heap/Boj11286.java
@@ -1,5 +1,33 @@
 package heap;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
 public class Boj11286 {
 
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N=Integer.parseInt(br.readLine());
+    PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+      if(Math.abs(a)==Math.abs(b)){
+        return Integer.compare(a,b);
+      }
+      return Integer.compare(Math.abs(a),Math.abs(b));
+    });
+    for(int i=0;i<N;i++){
+      int num=Integer.parseInt(br.readLine());
+      if(num!=0){
+        pq.offer(num);
+      }else{
+        if(pq.size()==0){
+          System.out.println("0");
+        }else{
+          System.out.println(pq.poll());
+        }
+
+      }
+    }
+  }
 }
