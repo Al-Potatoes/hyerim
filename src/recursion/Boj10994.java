4c858b6316e754c4d330bdd2db6992fe72430494
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-01-23 00:07:26 +0900

    [Refactor] 10994번: 별찍기-19

--- src/recursion/Boj10994.java
+++ src/recursion/Boj10994.java
@@ -7,25 +7,27 @@ import java.io.InputStreamReader;
 public class Boj10994 {
   static int N;
   static char arr[][];
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     N=Integer.parseInt(br.readLine());
-    arr=new char[4*N-3][4*N-3];
+    int size=4*N-3;
+    arr=new char[size][size];
 
-    star(0,0,N);
-    StringBuilder sb=new StringBuilder();
-    for(int i=0;i<4*N-3;i++){
-      for(int j=0;j<4*N-3;j++){
-        sb.append(arr[i][j] != 0 ? arr[i][j] : ' ');
+    star(0,0,size);
+
+    print(size);
+  }
+
+  private static void print(int size) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        sb.append(arr[i][j] != 0 ? arr[i][j] : " ");
       }
       sb.append("\n");
     }
-    for(int i=0;i<4*N-3;i++){
-      for(int j=0;j<4*N-3;j++){
-        System.out.print(arr[i][j] != 0 ? arr[i][j] : ' ');
-      }
-      System.out.print("\n");
-    }
+    System.out.println(sb.toString());
   }
 
   public static void star(int x,int y,int size){
@@ -33,16 +35,12 @@ public class Boj10994 {
       arr[x][y] = '*';
       return;
     }
-    for(int i=x;i<x+(4*size-3);i++){
-      for(int j=y;j<y+(4*size-3);j++){
-        if(i==x||i==x+(4*size-3)-1){
-          arr[i][j] = '*';
-        }
-        if(j==y||j==y+(4*size-3)-1){
-          arr[i][j] = '*';
-        }
-      }
+    for(int i=0;i<size;i++){
+      arr[x][y+i]='*';
+      arr[x+i][y]='*';
+      arr[x+size-1][y+i]='*';
+      arr[x+i][y+size-1]='*';
     }
-    star(x+2,y+2,size-1);
+    star(x+2,y+2,size-4);
   }
 }
