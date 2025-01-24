dd0840368d876a67629673321ca0e334ab77ac17
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-01-25 03:53:50 +0900

    [Refactor] 2014번: 소수의곱

--- src/heap/Boj2014.java
+++ src/heap/Boj2014.java
@@ -3,7 +3,6 @@ package heap;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -17,24 +16,20 @@ public class Boj2014 {
 
     int[] arr = new int[K];
     PriorityQueue<Long> pq = new PriorityQueue<>();
-    HashSet<Long> set = new HashSet<>();
 
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < K; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
       pq.offer((long) arr[i]);
-      set.add((long) arr[i]);
     }
 
     long answer = 0;
     for (int cnt = 0; cnt < N; cnt++) {
       answer = pq.poll();
+
       for (int i = 0; i < K; i++) {
         long next = answer * arr[i];
-        if (!set.contains(next)) {
-          pq.offer(next);
-          set.add(next);
-        }
+        pq.offer(next);
         if (answer % arr[i] == 0) break;
       }
     }
