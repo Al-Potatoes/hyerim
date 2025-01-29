fb55584f5fdd0bb90e8b1b68db3172c2776faec0
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-01-30 02:03:12 +0900

    [Add] 1655번: 가운데를말해요

--- src/heap/Boj1655.java
+++ src/heap/Boj1655.java
@@ -1,4 +1,37 @@
 package heap;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
 public class Boj1655 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N=Integer.parseInt(br.readLine());
+
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    StringBuilder sb=new StringBuilder();
+
+    for (int i = 0; i < N; i++) {
+      int num = Integer.parseInt(br.readLine());
+
+      if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+        maxHeap.offer(num);
+      } else {
+        minHeap.offer(num);
+      }
+
+      if (maxHeap.size() > minHeap.size() + 1) {
+        minHeap.offer(maxHeap.poll());
+      } else if (minHeap.size() > maxHeap.size()) {
+        maxHeap.offer(minHeap.poll());
+      }
+
+      sb.append(maxHeap.peek()).append("\n");
+    }
+
+    System.out.print(sb);
+  }
 }
