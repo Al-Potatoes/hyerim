9486a0e3c17dc2f46eeb46a515ad7ca1e618647c
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-02-17 03:51:35 +0900

    [Refactor] 5464번: 주차장

--- src/queue/Boj5464.java
+++ src/queue/Boj5464.java
@@ -4,64 +4,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Boj5464 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st=new StringTokenizer(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
     int N = Integer.parseInt(st.nextToken());
     int M = Integer.parseInt(st.nextToken());
-    int sum=0;
-    int []weights = new int[M];
-    int []rates = new int[N];
-    int []spaces = new int[N];
+
+    int[] rates = new int[N + 1];
+    int[] weights = new int[M + 1];
+    int[] parkingCar = new int[M + 1];
+
+    PriorityQueue<Integer> emptySpace = new PriorityQueue<>();
     Queue<Integer> waiting = new LinkedList<>();
 
-    for(int i=0;i<N;i++){
-      rates[i]=Integer.parseInt(br.readLine());
+    for (int i = 1; i <= N; i++) {
+      rates[i] = Integer.parseInt(br.readLine());
+      emptySpace.add(i);
     }
-    for(int i=0;i<M;i++){
-      weights[i]=Integer.parseInt(br.readLine());
+
+    for (int i = 1; i <= M; i++) {
+      weights[i] = Integer.parseInt(br.readLine());
     }
-    for(int i=0;i<2*M;i++){
-      int car=Integer.parseInt(br.readLine());
 
-      if(car>0){
-        boolean parked=false;
+    int sum = 0;
 
-        for(int j=0;j<N;j++){
-          if(spaces[j]==0){
-            spaces[j]=car;
-            sum+=rates[j]*weights[car-1];
-            parked=true;
-            break;
-          }
-        }
+    for (int i = 0; i < 2 * M; i++) {
+      int car = Integer.parseInt(br.readLine());
 
-        if(!parked){
+      if (car > 0) {
+        if (!emptySpace.isEmpty()) {
+          int idx = emptySpace.poll();
+          parkingCar[car] = idx;
+        } else {
           waiting.add(car);
         }
-      }else{
-        car=Math.abs(car);
+      } else {
+        car = -car;
+        int idx = parkingCar[car];
+        sum += weights[car] * rates[idx];
 
-        for(int j=0;j<N;j++){
-          if(spaces[j]==car){
-            spaces[j]=0;
-            break;
-          }
-        }
-
-        if(!waiting.isEmpty()){
-          int next=waiting.poll();
-          for(int j=0;j<N;j++){
-            if(spaces[j]==0){
-              spaces[j]=next;
-              sum+=rates[j]*weights[next-1];
-              break;
-            }
-          }
+        if (!waiting.isEmpty()) {
+          parkingCar[waiting.poll()] = idx;
+        } else {
+          emptySpace.add(idx);
         }
       }
     }
@@ -70,4 +61,4 @@ public class Boj5464 {
   }
 }
 
-// 132ms
+// 120ms
