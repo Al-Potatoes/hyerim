57e757414d2168e97061485c34a2bf5e9bddcdba
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-02-18 03:39:55 +0900

    [Refactor][6주차] 9372번: 상근이의여행

--- src/graph/Boj9372.java
+++ src/graph/Boj9372.java
@@ -1,27 +1,76 @@
 package graph;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Boj9372 {
+  static List<List<Integer>> graph;
+  static boolean[] visited;
+  static int N;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     int T = Integer.parseInt(br.readLine());
-    for (int i = 0; i < T; i++) {
+
+    for (int t = 0; t < T; t++) {
       StringTokenizer st = new StringTokenizer(br.readLine());
-      int N = Integer.parseInt(st.nextToken());
+      N = Integer.parseInt(st.nextToken());
       int M = Integer.parseInt(st.nextToken());
-      for (int j = 0; j < M; j++) {
+
+      graph = new ArrayList<>();
+      visited = new boolean[N + 1];
+
+      for (int i = 0; i <= N; i++) {
+        graph.add(new ArrayList<>());
+      }
+
+      for (int i = 0; i < M; i++) {
         st = new StringTokenizer(br.readLine());
         int u = Integer.parseInt(st.nextToken());
         int v = Integer.parseInt(st.nextToken());
+
+        graph.get(u).add(v);
+        graph.get(v).add(u);
       }
-      System.out.println(N-1);
+
+      int answer = bfs(1);
+      bw.write(answer + "\n");
     }
+
+    bw.flush();
+    bw.close();
+  }
+
+  public static int bfs(int V) {
+    Queue<Integer> q = new LinkedList<>();
+    q.add(V);
+    visited[V] = true;
+
+    int cnt = 0;
+
+    while (!q.isEmpty()) {
+      int now = q.poll();
+      for (int next : graph.get(now)) {
+        if (!visited[next]) {
+          visited[next] = true;
+          q.add(next);
+          cnt++;
+        }
+      }
+    }
+
+    return cnt;
   }
 }
 
-//328
+// 시간 428
