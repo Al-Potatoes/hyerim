10dcf7ec228751c57782f0ac4ab08260d705e3f5
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-10 14:18:09 +0900

    [Refactor][7주차] 25516번: 사과 수확하기

--- src/tree/Boj25516.java
+++ src/tree/Boj25516.java
@@ -4,6 +4,77 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Boj25516 {
+  static int n;
+  static int k;
+  static int[] apples;
+  static List<Integer>[] tree;
+  static boolean[] visited;
+  static int appleCount = 0;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    n = Integer.parseInt(st.nextToken());
+    k = Integer.parseInt(st.nextToken());
+
+    visited = new boolean[n];
+    apples = new int[n];
+    tree = new ArrayList[n];
+    for (int i = 0; i < n; i++) {
+      tree[i] = new ArrayList<>();
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+      st = new StringTokenizer(br.readLine());
+      int p = Integer.parseInt(st.nextToken());
+      int c = Integer.parseInt(st.nextToken());
+      tree[p].add(c);
+      tree[c].add(p);
+    }
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      apples[i] = Integer.parseInt(st.nextToken());
+    }
+
+    dfs(0, 0);
+
+    System.out.println(appleCount);
+  }
+
+  static void dfs(int node, int depth) {
+    visited[node] = true;
+
+    if (apples[node] == 1) {
+      appleCount++;
+    }
+
+    if (depth == k) {
+      return;
+    }
+
+    for (int next : tree[node]) {
+      if (!visited[next]) {
+        dfs(next, depth + 1);
+      }
+    }
+  }
+}
+
+// 460ms
+
+/*
+package tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -67,3 +138,4 @@ public class Boj25516 {
 }
 
 // 시간 652ms
+*/
