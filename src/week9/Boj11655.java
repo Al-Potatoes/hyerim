4154e8c56d3bf10bc51bf6a7a00b118cb4db5d47
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-04-03 14:13:23 +0900

    [Add][9주차] 11655번: ROT13

---
+++ src/week9/Boj11655.java
@@ -0,0 +1,31 @@
+package week9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj11655 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String S = br.readLine();
+    StringBuilder sb = new StringBuilder();
+
+    for(char c : S.toCharArray()) {
+      if (Character.isUpperCase(c)) {
+        sb.append((char) (((c - 'A') + 13) % 26 + 'A'));
+      }
+      else if(Character.isLowerCase(c)) {
+        sb.append((char) (((c - 'a') + 13) % 26 + 'a'));
+      }
+      else{
+        sb.append(c);
+      }
+    }
+
+    System.out.println(sb);
+  }
+}
+
+// 메모리 14188KB
+// 시간 104ms
