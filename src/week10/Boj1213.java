abf2a74430d286ffc1685689f67c5d694500f98f
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-05-01 18:03:33 +0900

    [Refactor][10주차] 1213번: 팰린드롬 만들기

--- src/week10/Boj1213.java
+++ src/week10/Boj1213.java
@@ -8,47 +8,42 @@ public class Boj1213 {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    char[] X = br.readLine().toCharArray();
-    StringBuilder Y = new StringBuilder();
-    StringBuilder result = new StringBuilder();
-    int[] alpabet = new int[26];
-    int oddCnt = 0;
-    int oddIdx = 0;
+    char[] input = br.readLine().toCharArray();
+    int[] counts = new int[26];
 
-    for (int i = 0; i < X.length; i++) {
-      alpabet[X[i] - 'A']++;
+    for (char c : input) {
+      counts[c - 'A']++;
     }
 
+    int oddCount = 0;
+    int oddIndex = -1;
+    StringBuilder half = new StringBuilder();
+
     for (int i = 0; i < 26; i++) {
-      if (alpabet[i] > 0 && alpabet[i] % 2 == 0) {
-        for (int j = 0; j < alpabet[i] / 2; j++) {
-          Y.append((char) (i + 'A'));
-        }
-      } else if (alpabet[i] > 0 && alpabet[i] % 2 != 0) {
-        if(alpabet[i] / 2 > 0) {
-          for(int j = 0; j < alpabet[i] / 2; j++) {
-            Y.append((char) (i + 'A'));
-          }
-        }
-        oddCnt++;
-        oddIdx = i;
+      if (counts[i] % 2 != 0) {
+        oddCount++;
+        oddIndex = i;
+      }
+      for (int j = 0; j < counts[i] / 2; j++) {
+        half.append((char) (i + 'A'));
       }
     }
 
-    result.append(Y);
-    if (oddCnt == 1) {
-      result.append((char) (oddIdx + 'A'));
-    }
-    result.append(Y.reverse());
-
-    if (oddCnt > 1) {
+    if (oddCount > 1) {
       System.out.println("I'm Sorry Hansoo");
-    } else {
-      System.out.println(result);
+      return;
     }
 
+    StringBuilder result = new StringBuilder();
+    result.append(half);
+    if (oddCount == 1) {
+      result.append((char) (oddIndex + 'A'));
+    }
+    result.append(half.reverse());
+
+    System.out.println(result);
   }
 }
 
-// 메모리 14264KB
-// 시간 104ms
+// 메모리 14208KB
+// 시간 108ms
