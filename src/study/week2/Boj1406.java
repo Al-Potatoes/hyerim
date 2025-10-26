f35b2c901a4699a047ffab967cbd6df40a0b4236
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-10-27 03:00:14 +0900

    refactor: 1406번 에디터

--- src/study/week2/Boj1406.java
+++ src/study/week2/Boj1406.java
@@ -1,59 +1,44 @@
 package study.week2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Boj1406 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        int M = Integer.parseInt(br.readLine());
-        Stack<Character> leftStack = new Stack<>();
-        Stack<Character> rightStack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            leftStack.push(s.charAt(i));
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> left = new Stack<>();
+        Stack<Character> right = new Stack<>();
+
+        String input = br.readLine();
+        for (int i = 0; i < input.length(); i++) {
+            left.push(input.charAt(i));
         }
-        for (int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            char kind = st.nextToken().charAt(0);
-            switch (kind) {
-                case 'L':
-                    if (!leftStack.empty()) {
-                        rightStack.push(leftStack.pop());
-                    }
-                    break;
-                case 'D':
-                    if (!rightStack.isEmpty()) {
-                        leftStack.push(rightStack.pop());
-                    }
-                    break;
-                case 'B':
-                    if (!leftStack.isEmpty()) {
-                        leftStack.pop();
-                    }
-                    break;
-                case 'P':
-                    char c = st.nextToken().charAt(0);
-                    leftStack.push(c);
-                    break;
+
+        int m = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            String line = br.readLine();
+            char cmd = line.charAt(0);
+
+            if (cmd == 'L') {
+                if (!left.isEmpty()) right.push(left.pop());
+            } else if (cmd == 'D') {
+                if (!right.isEmpty()) left.push(right.pop());
+            } else if (cmd == 'B') {
+                if (!left.isEmpty()) left.pop();
+            } else { // 'P x'
+                left.push(line.charAt(2));
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        while (!leftStack.isEmpty()) {
-            rightStack.push(leftStack.pop());
-        }
+        while (!left.isEmpty()) right.push(left.pop());
+        while (!right.isEmpty()) sb.append(right.pop());
 
-        while (!rightStack.isEmpty()) {
-            sb.append(rightStack.pop());
-        }
         System.out.println(sb);
     }
 }
 
+
 //TODO: 스택을 두 개 만들고 커서를 움직였을 경우 다른 스택에 옮겨뒀다가 다시 pop하기
-// 메모리 144160KB 시간 588ms
+// 메모리 73232KB 시간 448ms
