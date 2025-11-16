605c2694aaa60ab8bb7ca101a1265f26192940fe
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-11-17 01:25:00 +0900

    feat: 2748번 피보나치 수2

---
+++ src/study/week5/Boj2748.java
@@ -0,0 +1,24 @@
+package study.week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj2748 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(fibonachi(0,1, n));
+    }
+
+    public static long fibonachi(long a, long b, int n){
+        if(n==0){
+            return a;
+        }
+        return fibonachi(b,a+b,n-1);
+    }
+}
+
+// 메모리 14236KB 시간 112ms
